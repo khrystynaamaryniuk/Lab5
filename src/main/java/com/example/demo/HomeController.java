9dcfadd3eebb16aeabd2f8cda5e7cf7bca8a0af2
khrystynaamaryniuk <khrystyna.maryniuk@uconn.edu>
2024-11-05 13:15:50 -0500

    Add files via upload

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,8 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomeController {
+    @GetMapping("/")
+public String home() { return "home"; // for home.html
+    }
+}
